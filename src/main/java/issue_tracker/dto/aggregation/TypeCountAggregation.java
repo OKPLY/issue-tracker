11e9a77eabade0bf1383405af2c1e362dddd5a6f
Bitsu Mamo <bitsua.mamo@gmail.com>
2023-07-15 18:22:48 -0500

    Add Aggregation class for type and count

--- src/main/java/issue_tracker/dto/aggregation/TypeCountAggregation.java
+++ src/main/java/issue_tracker/dto/aggregation/TypeCountAggregation.java
@@ -1,2 +1,14 @@
-package issue_tracker.dto.aggregation;public class TypeCountAggregation {
+package issue_tracker.dto.aggregation;
+
+import lombok.Data;
+
+@Data
+public class TypeCountAggregation {
+    private String type;
+    private Long count;
+
+    public TypeCountAggregation(String type, Long count) {
+        this.type = type;
+        this.count = count;
+    }
 }
