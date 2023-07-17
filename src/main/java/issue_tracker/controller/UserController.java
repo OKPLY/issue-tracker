828671d52ca354c355a55d3b5ca00cb8dbe14fa3
Bitsu Mamo <bitsua.mamo@gmail.com>
2023-07-17 00:37:19 -0500

    Create End Point to return current Basic User

--- src/main/java/issue_tracker/controller/UserController.java
+++ src/main/java/issue_tracker/controller/UserController.java
@@ -2,7 +2,9 @@ package issue_tracker.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import issue_tracker.dto.aggregation.CreatedResolvedReviewedAggregate;
+import issue_tracker.dto.user.BasicUserDto;
 import issue_tracker.service.UserService;
+import issue_tracker.utility.Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final Util util;
    @GetMapping("/aggregate")
     public ResponseEntity<CreatedResolvedReviewedAggregate> aggregate() {
        return ResponseEntity.ok(userService.currentUserAggregate());
    }
+
+   //TODO: Make a dto to return only the necessary fields
+    @GetMapping
+    public ResponseEntity<BasicUserDto> getUser() {
+        return ResponseEntity.ok(userService.getCurrentUser());
+    }
 }
