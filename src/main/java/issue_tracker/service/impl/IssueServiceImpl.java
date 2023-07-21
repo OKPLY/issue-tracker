287ac6d207c557b0969b4da7bad912b86cabbd19
Natty <nattynigatu214.arg@gmail.com>
2023-07-20 19:01:03 -0500

    fix resolver filter

--- src/main/java/issue_tracker/service/impl/IssueServiceImpl.java
+++ src/main/java/issue_tracker/service/impl/IssueServiceImpl.java
@@ -221,7 +221,7 @@ public class IssueServiceImpl implements IssueService {
             issueStream = issueStream.filter((issue) -> issue.getReviewer() != null && issue.getReviewer().getId().equals(reviewerId));
 
         if (resolverId != null)
-            issueStream = issueStream.filter((issue) -> issue.getReviewer() != null && issue.getResolver().getId().equals(resolverId));
+            issueStream = issueStream.filter((issue) -> issue.getResolver() != null && issue.getResolver().getId().equals(resolverId));
 
         return issueStream.sorted((x, y) -> Long.compare(y.getId(), x.getId())).toList();
 
